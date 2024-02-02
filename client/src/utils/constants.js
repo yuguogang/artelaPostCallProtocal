7d7fcbeb21630849cf91b3c26ffaef726817c171
yuguogang <guoganyu313@gmail.com>
2024-02-02 10:22:33 +0800

    init

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+//0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA;
+export const contractAddress = "0x8F896bd4Ec7AC0B3CE4f0124D195f754d48b161A";
+export const contractABI = abi.abi;
